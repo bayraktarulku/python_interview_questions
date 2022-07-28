e35be3165572405475b4f35c8a4d64f8f0a7c6d8
ulku bayraktar <ulkubayraktar@gmail.com>
2022-07-28 17:16:51 +0300

    question-answer game

---
+++ basic_questions/magic8.py
@@ -0,0 +1,52 @@
+import random
+
+answers = [
+    "It is certain",
+    "It is decidedly so",
+    "Without a doubt",
+    "Yes – definitely",
+    "You may rely on it",
+    "As I see it, yes",
+    "Most likely",
+    "Outlook good",
+    "Yes Signs point to yes",
+    "Reply hazy",
+    "try again",
+    "Ask again later",
+    "Better not tell you now",
+    "Cannot predict now",
+    "Concentrate and ask again",
+    "Dont count on it",
+    "My reply is no",
+    "My sources say no",
+    "Outlook not so good",
+    "Very doubtful",
+]
+
+print("Hello World")
+print(
+    "I'm a Magic 8 Ball simulation. I answer your questions. Of course with my own technique^^."
+)
+
+
+def magic_eight_ball():
+    print("Ask me a question.")
+    input()
+    print(answers[random.randint(0, len(answers) - 1)])
+    print("Good luck")
+    replay()
+
+
+def replay():
+    print("Do you have another question? [Y/N] ")
+    reply = input()
+    if reply == "Y":
+        magic_eight_ball()
+    elif reply == "N":
+        exit()
+    else:
+        print("What nonsense questions are these? How old are you?")
+        replay()
+
+
+magic_eight_ball()
